e338b55aa7117c75f3879e6bb3f041976f370926
worsleyj <josephworsley23@gmail.com>
2024-07-11 13:48:43 -0400

    loop practice

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-const cars = ["One", "Two", "Three"];
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+// for (let i = 0; i < cats.length; i++) {
+//     console.log(cats[i]);
+// }
 
-console.log(cars.unshift("Zero"));
-console.log(cars);
+for (const item of cats) {
+    console.log(item);
+}
