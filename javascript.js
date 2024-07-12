eb88e85932983f6151c752dbcf0c7cd49fa2e735
worsleyj <josephworsley23@gmail.com>
2024-07-12 14:26:10 -0400

    dom manipulation practice, buttons

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function(fahrenheit) {
-    return Math.round((fahrenheit-32)*(5/9)*10)/10;
-};
+const container = document.querySelector("#container");
 
-const convertToFahrenheit = function(celsius) {
-    return Math.round(((celsius*(5/9))+32)*10)/10;
-};
+const display = document.querySelector(".display");
 
+const div = document.createElement("div");
+const div2 = document.createElement("div2");
 
-console.log(convertToFahrenheit(0))
-console.log(convertToCelsius(100));
+div.textContent = "DIV"
+div2.textContent = "DIV2"
+
+display.appendChild(div);
+display.appendChild(div2);
+
+div2.style.color = "red";
+div.style.cssText= "color: blue; background: grey;";
+
+const buttonTwo = document.querySelector("#two");
+buttonTwo.onclick = () => alert("hello World 2");
+
+const buttonThree = document.querySelector("#three");
+buttonThree.addEventListener("click", () => {
+    alert("hello WORLD 3");
+})
