05976dbdd88557b470e58149a2dfe29c65f1ebde
worsleyj <josephworsley23@gmail.com>
2024-07-16 15:23:19 -0400

    isEmpty

--- javascript.js
+++ javascript.js
@@ -1,26 +1,9 @@
-const ul = document.querySelector("ul")
-const input = document.querySelector("input")
-const button = document.querySelector("button")
+function isEmpty(object) {
+    for(let key in object) {
+        return false;
+    }
+    return true;
+}
 
-button.addEventListener("click", () => {
-    let currInput = input.value;
-    // console.log(currInput);
-
-    const li = document.createElement("li")
-    const delButton = document.createElement("button")
-    const span = document.createElement("span")
-
-    li.appendChild(span)
-    li.appendChild(delButton)
-
-    span.textContent = currInput;
-    delButton.textContent = "Delete";
-    delButton.addEventListener("click", () => {
-        li.remove()
-        input.focus();
-    })
-
-    ul.appendChild(li);
-    input.value = "";
-    input.focus();
-})
+let user = {}
+console.log(isEmpty(user));
