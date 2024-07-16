278352df55c722dd2d910075936ffb182ece32fe
worsleyj <josephworsley23@gmail.com>
2024-07-16 16:39:06 -0400

    calculator exercises completed

--- javascript.js
+++ javascript.js
@@ -1,55 +1,27 @@
-function sumOfTripledEvens(array) {
-    sum = 0;
-    for (let i = 0; i < array.length;i++) {
-        if (array[i] % 2 == 0) {
-            let tripledValued = array[i] *  3;
-            sum += tripledValued;
-        }
+const sum = function(array) {
+    const total = array.reduce((total, current) => {
+      return total + current;
+    })
+    return total
+  };
+
+console.log(sum([1, 2, 3]));
+
+const multiply = function(array) {
+    const multiplied = array.reduce((total, current) => {
+      return total * current;
+    })
+    return multiplied;
+  };
+
+console.log(multiply([1,2,3,4]));
+
+const factorial = function(num) {
+    let factorial = 1;
+    for (let i = 0; i < num; i++) {
+      factorial *= num-i;
     }
-    return sum;
-}
+    return factorial;
+  };
 
-console.log([sumOfTripledEvens([1, 2, 4, 6])]);
-
-function addOne(num) {
-    if (num % 2 ==0 ) {
-        return num * 3;
-    } else return num;
-}
-
-const arr = [1, 2 ,3 , 4 , 5];
-const mappedArr = arr.map(addOne)
-console.log(mappedArr);
-
-function isOdd(num) {
-    return num % 2 !== 0;
-}
-
-
-const oddNums = arr.filter(isOdd)
-console.log(oddNums);
-console.log();
-
-const productOfAllNums = arr.reduce((total, currItem) => {
-    return total * currItem;
-}, 1);
-
-console.log(productOfAllNums);
-
-function isEven(num) {
-    return num%2 ==0;
-}
-
-function tripleNum(num) {
-    return num*3;
-}
-
-function sumOfTripledEvensRewrite(array) {
-    const evenNums = array.filter(isEven);
-    const tripledNums = evenNums.map(tripleNum);
-    const sumOfTripledNums = tripledNums.reduce((total, currItem) => total + currItem)
-    console.log(sumOfTripledNums);
-    return sumOfTripledNums;
-}
-
-console.log(sumOfTripledEvensRewrite([1, 2, 4 ,6]));
+console.log(factorial(5));
