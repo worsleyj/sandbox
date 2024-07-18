7d91f9852fbec686d46322dd5b95f2a6f0958c5a
worsleyj <josephworsley23@gmail.com>
2024-07-18 13:31:25 -0400

    fixed 0 and 1 values

--- javascript.js
+++ javascript.js
@@ -2,16 +2,18 @@ const fibonacci = function(index) {
   index = parseInt(index);
   if (index < 0) {
     return "OOPS";
+  } else if (index == 0 || index == 1) {
+    return index;
   }
 
   fibonacciArray = [1, 1]
   for (let i = 1; i < index-1; i++) {
     nextValue = fibonacciArray[i] + fibonacciArray[i-1];
-    fibonacciArray.push(nextValue)
+    fibonacciArray.push(nextValue);
   }
   return fibonacciArray[index-1];
 };
 
 
-console.log(fibonacci("4"));
+console.log(fibonacci("0"));
 console.log(fibonacci(25));
