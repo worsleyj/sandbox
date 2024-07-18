7615e232761117db32cbfc0e427cb00991a38de1
worsleyj <josephworsley23@gmail.com>
2024-07-18 13:48:07 -0400

    get titles of book

--- javascript.js
+++ javascript.js
@@ -1,19 +1,6 @@
-const fibonacci = function(index) {
-  index = parseInt(index);
-  if (index < 0) {
-    return "OOPS";
-  } else if (index == 0 || index == 1) {
-    return index;
-  }
-
-  fibonacciArray = [1, 1]
-  for (let i = 1; i < index-1; i++) {
-    nextValue = fibonacciArray[i] + fibonacciArray[i-1];
-    fibonacciArray.push(nextValue);
-  }
-  return fibonacciArray[index-1];
+const getTheTitles = function(bookArr) {
+  let titleArr = bookArr.map((book) => book.title);
 };
 
-
-console.log(fibonacci("0"));
-console.log(fibonacci(25));
+// Do not edit below this line
+module.exports = getTheTitles;
