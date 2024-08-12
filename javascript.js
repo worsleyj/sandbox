3b5ded43cab36e77c82bc521975a64521fb599c2
worsleyj <josephworsley23@gmail.com>
2024-08-12 14:38:45 -0400

    fixes

--- javascript.js
+++ javascript.js
@@ -27,3 +27,13 @@ const newUserTwo = createUser("AParkedChopper");
 newUserTwo.giveReputation();
 newUserTwo.giveReputation();
 console.log(newUserTwo.getReputation());
+
+const calculator = (function () {
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {add, sub, mul, div}
+})();
+
+console.log(calculator.add(3, 5));
