7bb194f5e078b6acf22c742f34989d00cf593b0b
worsleyj <josephworsley23@gmail.com>
2024-08-13 14:02:23 -0400

    added template.html via webpack

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,4 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
