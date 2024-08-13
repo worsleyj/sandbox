2bfaa40b27b5135fd555adfacca1b45f322e1f68
worsleyj <josephworsley23@gmail.com>
2024-08-13 14:15:39 -0400

    add webpack-dev-server

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
@@ -20,6 +24,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
