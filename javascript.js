ef66855bc7f80c3196ed3714b336dc27bdef0f02
worsleyj <josephworsley23@gmail.com>
2024-08-23 13:35:31 -0400

    reset

--- javascript.js
+++
@@ -1,8 +0,0 @@
-const dropdown = document.querySelector(".dropdown");
-const dropdownItems = document.querySelector(".dropdown-items");
-dropdown.addEventListener("mouseover", () => {
-  dropdownItems.style.display = "block";
-});
-dropdown.addEventListener("mouseout", () => {
-  dropdownItems.style.display = "none";
-});
