fa9e33386d990f1fe941fddb5446a9684f10ac45
worsleyj <josephworsley23@gmail.com>
2024-08-26 16:21:32 -0400

    add finally and then

--- javascript.js
+++ javascript.js
@@ -4,3 +4,9 @@ let promise = new Promise(function (resolve, reject) {
   // after 1 second signal that the job is done with the result "done"
   setTimeout(() => resolve("done"), 1000);
 });
+promise.finally(() => console.log("completed"));
+
+promise.then(
+  (result) => alert(result), // doesn't run
+  (error) => alert(error)
+);
