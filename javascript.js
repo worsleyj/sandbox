32431903e13c0ed509539fd829948d5270ca77b1
worsleyj <josephworsley23@gmail.com>
2024-08-26 17:15:01 -0400

    add button to generate new gif each time

--- javascript.js
+++ javascript.js
@@ -1,11 +1,20 @@
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=6ai0C1nbu2BfXTGjoBtsrH3wUBeiqcqF&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+
+function newCatGif(searchTerm) {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=6ai0C1nbu2BfXTGjoBtsrH3wUBeiqcqF&s=" +
+      searchTerm,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.data.images.original.url);
+      img.src = response.data.images.original.url;
+    });
+}
+const newGifBtn = document.querySelector("button");
+newGifBtn.addEventListener("click", () => {
+  newCatGif("cats");
+});
