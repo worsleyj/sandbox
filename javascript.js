2468c7a95f9f3d19f0eec1ab8984e69f888e4d75
worsleyj <josephworsley23@gmail.com>
2024-08-27 12:17:12 -0400

    sumRange recursion

--- javascript.js
+++ javascript.js
@@ -1,25 +1,11 @@
-const img = document.querySelector("img");
-const searchBox = document.querySelector("input");
-const newGifBtn = document.querySelector("button");
-
-function newCatGif(searchTerm) {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=6ai0C1nbu2BfXTGjoBtsrH3wUBeiqcqF&s=" +
-      searchTerm,
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response.data.images.original.url);
-      img.src = response.data.images.original.url;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+function sumRange(num) {
+  if (num === 1) {
+    return num;
+  } else {
+    return num + sumRange(num - 1);
+  }
 }
 
-newGifBtn.addEventListener("click", () => {
-  newCatGif(searchBox.value);
-});
+console.log(sumRange(3));
+
+// 3 + 2 + 1
